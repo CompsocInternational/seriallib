858e9968fd385e3999c92829c08db27939e34459
Isaac - The456 <the456@the456gamer.dev>
2024-04-17 09:50:53 +0200

    unasync it. also read until the target thing, retyring on timeout. really doesnt like asycn. also makes it easy to use elsewhere

--- seriallib/armcontroller.py
+++ seriallib/armcontroller.py
@@ -1,4 +1,3 @@
-import asyncio
 from seriallib.consts import ATTEMPTS_MAX, MOCK_PORT_NAME
 from seriallib.commands import IncomingArmCommand, OutgoingArmCommand
 
@@ -9,7 +8,6 @@ import serial
 import time
 
 from seriallib.exceptions import (
-    ArmInUseException,
     ArmRetryLimitExceededException,
     ArmUnknownOrUnexpectedResponseException,
 )
@@ -19,115 +17,109 @@ class ArmController:
     def __init__(self, port: str):
         self.serial = None
         self.port = port
-        self.lock = asyncio.Lock()
 
         if self.port == MOCK_PORT_NAME:
             print("mocking output")
 
-    async def grab(self):
-        await self._write_command(OutgoingArmCommand.GRAB)
+    def grab(self):
+        self._write_command(OutgoingArmCommand.GRAB)
 
-    async def move_bin1(self):
-        await self._write_command(OutgoingArmCommand.MOVE_BIN1)
+    def move_bin1(self):
+        self._write_command(OutgoingArmCommand.MOVE_BIN1)
 
-    async def move_bin2(self):
-        await self._write_command(OutgoingArmCommand.MOVE_BIN2)
+    def move_bin2(self):
+        self._write_command(OutgoingArmCommand.MOVE_BIN2)
 
-    async def move_bin3(self):
-        await self._write_command(OutgoingArmCommand.MOVE_BIN3)
+    def move_bin3(self):
+        self._write_command(OutgoingArmCommand.MOVE_BIN3)
 
-    async def move_neutral(self):
-        await self._write_command(OutgoingArmCommand.RESET)
+    def move_neutral(self):
+        self._write_command(OutgoingArmCommand.RESET)
 
-    async def _write_command(self, command: OutgoingArmCommand) -> None:
+    def _write_command(self, command: OutgoingArmCommand) -> None:
         print(f"Sending command {command}")
         
-        if (self.lock.locked()):
-            raise ArmInUseException("Arm is currently in use: only one command can be sent at a time.")
-        
-        
-        async with self.lock:
-            if self.port == MOCK_PORT_NAME:
-                return
+        if self.port == MOCK_PORT_NAME:
+            return
 
-            done = False
-            attempts = 1
+        done = False
+        attempts = 1
 
 
-            while not done:
-                if attempts > ATTEMPTS_MAX:
-                    raise ArmRetryLimitExceededException(
-                        f"Failed to send command after {ATTEMPTS_MAX} attempts"
-                    )
-                try:
-                    (await self._get_serial()).write(command.value.encode())
-                    # if unwritten > 0:
-                    # raise serial.SerialException("Could not write all bytes")
+        while not done:
+            if attempts > ATTEMPTS_MAX:
+                raise ArmRetryLimitExceededException(
+                    f"Failed to send command after {ATTEMPTS_MAX} attempts"
+                )
+            try:
+                self._get_serial().write(command.value.encode())
+                # if unwritten > 0:
+                # raise serial.SerialException("Could not write all bytes")
+                done = True
+            except serial.SerialException as e:
+                print(e)
+                print(f"Failed to send command {command} attempt {attempts}, retrying")
+                attempts += 1
+                self.serial = None
+                time.sleep(1)
+
+        print(f"Completed sending command in {attempts} attempts")
+
+        # wait for command finished_ack
+        attempts = 1
+        done = False
+
+
+
+        while not done:
+            if attempts > ATTEMPTS_MAX:
+                raise ArmRetryLimitExceededException(
+                    f"Failed to rcv response after {ATTEMPTS_MAX} attempts"
+                )
+            try:
+                data = self._get_serial().read_until(IncomingArmCommand.ACK.value.encode()).decode()
+                if data == IncomingArmCommand.ACK.value:
                     done = True
-                except serial.SerialException as e:
-                    print(e)
-                    print(f"Failed to send command {command} attempt {attempts}, retrying")
-                    attempts += 1
-                    self.serial = None
-                    await asyncio.sleep(1)
+                else:
+                    raise ArmUnknownOrUnexpectedResponseException(
+                        f"Unexpected response '{data}' to command {command}"
+                    )
+            except serial.SerialException as e:
+                print(
+                    f"Failed to receive ack for command {command} attempt {attempts}, retrying\n{e}"
+                )
+                attempts += 1
+                self.serial = None
+                time.sleep(1)
 
-            print(f"Completed sending command in {attempts} attempts")
+        print(f"Received ack for command {command}")
 
-            # wait for command finished_ack
-            attempts = 1
-            done = False
+        attempts = 1
+        done = False
 
+        while not done:
+            if attempts > ATTEMPTS_MAX:
+                raise ArmRetryLimitExceededException(
+                    f"Failed to rcv success after {ATTEMPTS_MAX} attempts"
+                )
+            try:
+                data = self._get_serial().read_until(IncomingArmCommand.ACK.value.encode()).decode()
+                if data == IncomingArmCommand.FINISHED.value:
+                    done = True
+                else:
+                    raise ArmUnknownOrUnexpectedResponseException(
+                        f"Unexpected response '{data}' to command {command}"
+                    )
+            except serial.SerialException as e:
+                print(f"Failed to receive finished_ack for command {command} attempt {attempts}, retrying\n{e}")
+                attempts += 1
+                self.serial = None
+                time.sleep(1)
+
+        print("Received finished_ack for command")
     
 
-            while not done:
-                if attempts > ATTEMPTS_MAX:
-                    raise ArmRetryLimitExceededException(
-                        f"Failed to rcv response after {ATTEMPTS_MAX} attempts"
-                    )
-                try:
-                    response = (await self._get_serial()).read(1).decode()
-                    if response == IncomingArmCommand.ACK.value:
-                        done = True
-                    else:
-                        raise ArmUnknownOrUnexpectedResponseException(
-                            f"Unexpected response '{response}' to command {command}"
-                        )
-                except serial.SerialException:
-                    print(
-                        f"Failed to receive ack for command {command} attempt {attempts}, retrying"
-                    )
-                    attempts += 1
-                    self.serial = None
-                    await asyncio.sleep(1)
-
-            print(f"Received ack for command {command}")
-
-            attempts = 1
-            done = False
-
-            while not done:
-                if attempts > ATTEMPTS_MAX:
-                    raise ArmRetryLimitExceededException(
-                        f"Failed to rcv success after {ATTEMPTS_MAX} attempts"
-                    )
-                try:
-                    response = (await self._get_serial()).read(1).decode()
-                    if response == IncomingArmCommand.FINISHED.value:
-                        done = True
-                    else:
-                        raise ArmUnknownOrUnexpectedResponseException(
-                            f"Unexpected response '{response}' to command {command}"
-                        )
-                except serial.SerialException:
-                    print(f"Failed to receive finished_ack for command {command} attempt {attempts}, retrying")
-                    attempts += 1
-                    self.serial = None
-                    await asyncio.sleep(1)
-
-            print("Received finished_ack for command")
-        
-
-    async def _get_serial(self) -> serial.Serial:
+    def _get_serial(self) -> serial.Serial:
         """Internal: get underlying serial object, initializing if necessary
         DO NOT CALL THIS WHEN self.port == MOCK_PORT
 
@@ -136,12 +128,12 @@ class ArmController:
         """
         if self.serial is None:
             self.serial = self._serial_init()
-        return await self.serial
+        return self.serial
 
-    async def _serial_init(self) -> serial.Serial:
+    def _serial_init(self) -> serial.Serial:
         print("start serial init")
         s = serial.Serial(self.port, timeout=2)
-        await asyncio.sleep(1)
+        time.sleep(1)
         s.flush()
         s.reset_input_buffer()
         s.reset_output_buffer()
--- seriallib/testgui.py
+++ seriallib/testgui.py
@@ -1,63 +1,34 @@
-import tkinter as tk
-import asyncio
-from typing import Any, Callable, Coroutine
+import tkinter
+from typing import Callable
 
 import seriallib.armcontroller
 
 controller = seriallib.armcontroller.ArmController(
-    "/dev/serial/by-id/usb-Arduino__www.arduino.cc__0043_8513332303635140E1A0-if00"
+    "/dev/serial/by-id/usb-Arduino__www.arduino.cc__0043_8513332303635140E1A0-if00" # change for your port. this is correct for andre's jetson nano on isaacs laptop.
 )
 
 
-def wrap(func: Callable[[], Coroutine[Any,Any,None]]) -> Callable:
-    def wrapper():
-        asyncio.create_task(func())
-
-    return wrapper
+def wrap(func: Callable) -> Callable:
+    return func
 
 
-class App(tk.Tk):
-    
-    def __init__(self, loop, interval=1/60):
-        super().__init__()
-        self.loop = loop
-        self.protocol("WM_DELETE_WINDOW", self.close)
-        self.tasks = []
-        self.tasks.append(loop.create_task(self.setup()))
-        self.tasks.append(loop.create_task(self.updater(interval)))
+root = tkinter.Tk()
 
-    async def setup(self):
-        grab_button = tk.Button(self, text="Grab", command=wrap(controller.grab))
-        grab_button.pack()
+root.geometry("800x600")
 
-        bin1_button = tk.Button(self, text="Bin 1", command=wrap(controller.move_bin1))
-        bin1_button.pack()
+grab_button = tkinter.Button(root, text="Grab", command=wrap(controller.grab))
+grab_button.pack()
 
-        bin2_button = tk.Button(self, text="Bin 2", command=wrap(controller.move_bin2))
-        bin2_button.pack()
+bin1_button = tkinter.Button(root, text="Bin 1", command=wrap(controller.move_bin1))
+bin1_button.pack()
 
-        bin3_button = tk.Button(self, text="Bin 3", command=wrap(controller.move_bin3))
-        bin3_button.pack()
+bin2_button = tkinter.Button(root, text="Bin 2", command=wrap(controller.move_bin2))
+bin2_button.pack()
 
-        neutral_button = tk.Button(self, text="Neutral", command=wrap(controller.move_neutral))
-        neutral_button.pack()
-        
-        self.loop
-        
+bin3_button = tkinter.Button(root, text="Bin 3", command=wrap(controller.move_bin3))
+bin3_button.pack()
 
-    async def updater(self, interval):
-        while True:
-            self.update()
-            await asyncio.sleep(interval)
+home_button = tkinter.Button(root, text="Home", command=wrap(controller.move_neutral))
+home_button.pack()
 
-    def close(self):
-        for task in self.tasks:
-            task.cancel()
-        self.loop.stop()
-        self.destroy()
-
-
-loop = asyncio.get_event_loop()
-app = App(loop)
-loop.run_forever()
-loop.close()
+root.mainloop()
